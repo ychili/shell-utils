2a373df972ddec1a3183e745d76322d99ad3c058
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2023-04-21 15:15:07 -0400

    Initial commit

---
+++ logging/README.md
@@ -0,0 +1,98 @@
+# logging.bash
+
+A logging library for Bash
+
+Forked/derived from the [bashlog
+library](https://github.com/Zordrak/bashlog)
+
+## Usage
+
+Add the line `. /path/to/logging.bash` at the top of your script(s).
+
+Enable one or more handler functions with a line like
+`LOGGING_HANDLERS[logging_stderr]=${LOGGING_SEVERITIES[info]}`
+(for other examples see below).
+
+Call the function `log()` with a level name and a log message.
+For example:
+
+    log info 'My info message'
+    log warn 'My warning message'
+    log error 'My error message'
+    log debug 'My debugging message'
+
+## Standard Log Handlers
+
+Four log handler functions are defined as standard, but have to be
+enabled by setting the value of the handler’s name in the array
+LOGGING_HANDLERS to a numeric logging level.
+
+For example:
+
+    # Log messages of level 'warn' and above to standard error
+    LOGGING_HANDLERS[logging_stderr]=${LOGGING_SEVERITIES[warn]}
+    # Write messages of all levels to log file 
+    LOGGING_HANDLERS[logging_file]=${LOGGING_SEVERITIES[debug]}
+    # Enable syslog logging at level 'info'
+    LOGGING_HANDLERS[logging_syslog]=${LOGGING_SEVERITIES[info]}
+
+The array LOGGING_SEVERITIES contains the mapping between level names
+and numeric codes, same as defined in RFC 5424.
+
+### logging_stderr
+
+`logging_stderr()` writes log to standard error with the format
+`${prog}: ${level_name}: ${message}`.
+The value of `prog` can be customized with the environment variable
+LOGGING_PROG. It defaults to `basename $0`.
+
+### logging_file
+
+`logging_file()` writes log to the file named in the environment variable
+LOGGING_FILE_PATH.
+The default datestamp format is ‘+%F %T’ (as interpreted by **date**(1))
+and can be customized with the environment variable LOGGING_DATE_FORMAT.
+
+### logging_json
+
+`logging_json()` writes JSON-formatted log to the file named in the
+environment variable LOGGING_JSON_PATH.
+
+### logging_syslog
+
+`logging_syslog()` uses the **logger**(1) utility to log to syslog.
+The tag can be customized with the environment variable
+LOGGING_SYSLOG_TAG, which defaults to `basename $0`.
+The facility can be customized with the environment variable
+LOGGING_SYSLOG_FACILITY, which defaults to local0.
+
+## Custom Log Handlers
+
+The standard log handler functions’ behavior can be overridden by a
+defining a new function with the same name.
+Supplementary handlers can be added by enabling them in
+LOGGING_HANDLERS.
+
+## Bash Shell Options
+
+Quoting from the README for bashlog:
+
+> This library sets the following shell options:
+>
+> `set -uo pipefail`
+>
+> This is a recommended default for any scripts you write. You should
+> not use unbound variables (-u). You should ensure pipelines fail if
+> any of the commands in the pipeline fail (-o pipefail).
+
+### Bash Debug Trap
+
+> This library enables a bash DEBUG trap, that provides you with a
+> variable called `prev_cmd` to help you write simple and useful debug
+> logging messages.
+>
+> For example:
+>
+>     my_command --with very -c -o -m -p -l -e -x -- parameters \
+>       && log debug "${prev_cmd}" \
+>       || log error "${prev_cmd};
---
+++ logging/logging.bash
@@ -0,0 +1,151 @@
+set -uo pipefail
+
+LOGGING_SCRIPTNAME="$(basename "$0")"
+
+# Enable handlers in this array by setting the handler's name to the log level
+# its enabled for.
+declare -A LOGGING_HANDLERS
+
+declare -A LOGGING_SEVERITIES=(
+  ['emerg']=0
+  ['alert']=1
+  ['crit']=2
+  ['err']=3
+  ['warn']=4
+  ['notice']=5
+  ['info']=6
+  ['debug']=7
+)
+readonly LOGGING_SEVERITIES
+
+declare -a LOGGING_DISPLAY_NAMES=(
+  'EMERG'
+  'ALERT'
+  'CRITICAL'
+  'ERROR'
+  'WARNING'
+  'NOTICE'
+  'INFO'
+  'DEBUG'
+)
+
+logging_exception() {
+  printf "Logging Exception: %s\n" "$*" >&2
+}
+
+# function log (level, message, ...)
+#
+log() {
+  local log_level="$1"
+  shift
+  local line="$*"
+
+  local name; local -i severity
+  for name in "${!LOGGING_SEVERITIES[@]}"; do
+    if [[ $log_level =~ $name ]]; then
+      severity="${LOGGING_SEVERITIES[${name}]}"
+      break
+    fi
+  done
+  if [[ -z $severity ]]; then
+    log error "Undefined log level trying to log: ${log_level} ${line}"
+    return 1
+  fi
+
+  local handler; local -i level_enabled
+  for handler in "${!LOGGING_HANDLERS[@]}"; do
+    level_enabled="${LOGGING_HANDLERS[${handler}]:--1}"
+    if ((severity <= level_enabled)); then
+      "$handler" "$severity" "$line"
+    fi
+  done
+}
+
+# function logging_syslog (severity, message, ...)
+#
+logging_syslog() {
+  local severity="$1"
+  shift
+  local line="$*"
+
+  local tag="${LOGGING_SYSLOG_TAG:-${LOGGING_SCRIPTNAME}}"
+  local facility="${LOGGING_SYSLOG_FACILITY:-local0}"
+
+  local pid="$$"
+
+  local cmd=( logger
+              --id="${pid}" --tag="${tag}"
+              --priority="${facility}.${severity}"
+              "$line" )
+  if ! "${cmd[@]}"; then
+    logging_exception "${cmd[*]}"
+  fi
+}
+
+# function logging_file (severity, message, ...)
+#
+logging_file() {
+  local severity="$1"
+  shift
+  local line="$*"
+
+  local level_name="${LOGGING_DISPLAY_NAMES[${severity}]:-UNKNOWN}"
+
+  local file_path
+  if [[ ${LOGGING_FILE_PATH:-} ]]; then
+    file_path="$LOGGING_FILE_PATH"
+  else
+    local tmp_dir="${TMPDIR:-/tmp}"
+    file_path="${tmp_dir}/${LOGGING_SCRIPTNAME}.log"
+  fi
+  local date_format="${LOGGING_DATE_FORMAT:-+%F %T}"
+
+  local date
+  date="$(date "$date_format")"
+
+  local cmd=( printf '%s %s %s\n' "$date" "$level_name" "$line" )
+  if ! "${cmd[@]}" >> "$file_path"; then
+    logging_exception "${cmd[*]} >> ${file_path}"
+  fi
+}
+
+# function logging_json (severity, message, ...)
+#
+logging_json() {
+  local severity="$1"
+  shift
+  local line="$*"
+
+  local json_path
+  if [[ ${LOGGING_JSON_PATH:-} ]]; then
+    json_path="$LOGGING_JSON_PATH"
+  else
+    local tmp_dir="${TMPDIR:-/tmp}"
+    json_path="${tmp_dir}/${LOGGING_SCRIPTNAME}.log.json"
+  fi
+
+  local cmd=( printf '{"timestamp":%d,"level":%d,"message":"%s"}\n'
+              "$EPOCHSECONDS" "$severity" "$line" )
+  if ! "${cmd[@]}" >> "$json_path"; then
+    logging_exception "${cmd[*]} >> ${json_path}"
+  fi
+}
+
+# function logging_stderr (severity, message, ...)
+#
+logging_stderr() {
+  local severity="$1"
+  shift
+  local line="$*"
+
+  local prog="${LOGGING_PROG:-${LOGGING_SCRIPTNAME}}"
+  local level_name="${LOGGING_DISPLAY_NAMES[${severity}]:-UNKNOWN}"
+
+  printf '%s: %s: %s\n' "$prog" "$level_name" "$line" >&2
+}
+
+# shellcheck disable=SC2034
+declare prev_cmd="null"
+declare this_cmd="null"
+trap 'prev_cmd=$this_cmd; this_cmd=$BASH_COMMAND' DEBUG || \
+  logging_exception 'DEBUG trap failed to set'
